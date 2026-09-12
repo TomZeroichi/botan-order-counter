2112e4bfe26faf39cc2d53d39ca8290d6c7d4fa7
TOM <60422178+TomZeroichi@users.noreply.github.com>
2026-09-12 20:12:19 +0900

    Add service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="botan-order-v1-4";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
