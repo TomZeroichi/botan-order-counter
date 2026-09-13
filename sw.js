59c9ee59e1e27375c37b82542e6a8b12ba74f555
TOM <60422178+TomZeroichi@users.noreply.github.com>
2026-09-13 09:53:12 +0900

    Bump service worker cache to V1.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="botan-order-v1-4";
+const CACHE="botan-order-v1-5";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
